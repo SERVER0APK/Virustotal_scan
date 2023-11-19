9ca0690a7f6566325862e4f16baaa00c51ec0cc3
ali kadhim <ali.crazy60@gmail.com>
2023-11-20 02:33:30 +0300

    Add project files.

---
+++ WindowsApp5/Virustotal_scan.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Virustotal_scan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(47, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(12, 17)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(378, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(47, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(165, 43)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Uplode File"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("LBC", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(47, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(12, 84)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(377, 54)
+        Me.Label1.TabIndex = 7
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(26, Byte), Integer), CType(CType(26, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.RichTextBox2.Font = New System.Drawing.Font("LBC", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(250, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(47, Byte), Integer))
+        Me.RichTextBox2.Location = New System.Drawing.Point(7, 141)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(382, 371)
+        Me.RichTextBox2.TabIndex = 9
+        Me.RichTextBox2.Text = ""
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.RichTextBox1.Font = New System.Drawing.Font("LBC", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(440, 180)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(353, 332)
+        Me.RichTextBox1.TabIndex = 8
+        Me.RichTextBox1.Text = ""
+        Me.RichTextBox1.Visible = False
+        '
+        'Virustotal_scan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(34, Byte), Integer), CType(CType(34, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(392, 524)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Virustotal_scan"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Virustotal scan"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ WindowsApp5/Virustotal_scan.vb
@@ -0,0 +1,133 @@
+﻿Imports System.IO
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Security.Cryptography
+Imports System.Net
+Imports System.Text.RegularExpressions
+Public Class Virustotal_scan
+
+    Public Function CalculateFileHash(filePath As String) As String
+        Dim hashString As String = ""
+
+        Try
+            Using fileStream As FileStream = New FileStream(filePath, FileMode.Open, FileAccess.Read)
+                Using sha256 As SHA256 = SHA256.Create()
+                    Dim hashBytes As Byte() = sha256.ComputeHash(fileStream)
+                    hashString = BitConverter.ToString(hashBytes).Replace("-", String.Empty)
+                End Using
+            End Using
+        Catch ex As Exception
+        End Try
+
+        Return hashString
+    End Function
+
+    Public Async Sub GetFileDetails()
+        Dim client As HttpClient = New HttpClient()
+        Dim request As HttpRequestMessage = New HttpRequestMessage() With {
+        .Method = HttpMethod.Get,
+        .RequestUri = New Uri("https://www.virustotal.com/api/v3/files/" + TextBox1.Text)
+    }
+        request.Headers.Accept.Add(New MediaTypeWithQualityHeaderValue("application/json"))
+        request.Headers.Add("x-apikey", "478a62faa7503392d6dd4ffde5144178fbfd704aee911fbe8e811553ac4372af")
+
+        Try
+            Using response As HttpResponseMessage = Await client.SendAsync(request)
+                response.EnsureSuccessStatusCode()
+                Dim body As String = Await response.Content.ReadAsStringAsync()
+                RichTextBox1.Text = (body)
+                result()
+                prog()
+            End Using
+        Catch ex As Exception
+        End Try
+    End Sub
+    Sub result()
+        Dim regex As New Regex("""([^""]+)"":\s*{[^}]+,\s*""result"":\s*""([^""]+)""")
+        Dim matches As MatchCollection = regex.Matches(RichTextBox1.Text)
+        Dim results As New List(Of String)()
+        For Each match As Match In matches
+            Dim engineName As String = match.Groups(1).Value
+            Dim result As String = match.Groups(2).Value
+            results.Add(engineName & " ====>> " & result)
+        Next
+        RichTextBox2.Text = String.Join(Environment.NewLine, results)
+        Dim engineNameCount As Integer = matches.Count
+        Label1.Text = " The virus has been detected by " + engineNameCount.ToString() + " protection(s)."
+    End Sub
+    Public Structure VirusTotalScanResult
+        Public Property Vendor As String
+        Public Property Result As String
+    End Structure
+    Dim filePath As String
+    <STAThread> ' إضافة السمة STAThread
+    Sub filees()
+        Me.Invoke(Sub()
+                      Dim openFileDialog As New OpenFileDialog()
+                      openFileDialog.InitialDirectory = "C:\"
+                      openFileDialog.Filter = "Files (*.*)|*.*"
+                      If openFileDialog.ShowDialog() = DialogResult.OK Then
+                          filePath = openFileDialog.FileName
+                      Else
+                          filePath = String.Empty
+                      End If
+                      openFileDialog.Dispose()
+                  End Sub)
+    End Sub
+    Sub prog()
+        Dim labelText As String = Label1.Text
+        Dim index As Integer = labelText.IndexOf(":") + 1
+        Dim numberText As String = labelText.Substring(index).Trim()
+        Dim targetValue As Integer = Integer.Parse(numberText)
+        For i As Integer = 2 To targetValue
+            Application.DoEvents()
+        Next
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim thread As New Threading.Thread(AddressOf third_filees)
+
+        thread.Start()
+    End Sub
+    Sub third_filees()
+        '
+        filees()
+
+        Try
+            Dim client As New HttpClient()
+            ' Create a new MultipartFormDataContent object.
+            Dim content As New MultipartFormDataContent()
+            ' Add the file as a StreamContent parameter.
+            Dim fileStream As Stream = File.OpenRead(filePath)
+            Dim fileContent As New StreamContent(fileStream)
+            content.Add(fileContent, "file", Path.GetFileName(filePath))
+            ' Add the required headers.
+            Label1.Text = "Uploading"
+            client.DefaultRequestHeaders.Add("accept", "application/json")
+            client.DefaultRequestHeaders.Add("x-apikey", "478a62faa7503392d6dd4ffde5144178fbfd704aee911fbe8e811553ac4372af")
+            ' Send the request and get the response.
+            Dim response As HttpResponseMessage = client.PostAsync("https://www.virustotal.com/api/v3/files", content).Result
+            ' Check the response status code.
+            If response.StatusCode = HttpStatusCode.OK Then
+                ' Get the response body.
+                Dim responseBody As String = response.Content.ReadAsStringAsync().Result
+                ' Display the response in RichTextBox2.
+                RichTextBox2.Invoke(Sub() RichTextBox2.Text = responseBody)
+            Else
+                ' Print the error message.
+                RichTextBox2.Invoke(Sub() RichTextBox2.Text = "Error: " & response.ReasonPhrase)
+            End If
+            Dim hash As String = CalculateFileHash(filePath)
+            TextBox1.Invoke(Sub() TextBox1.Text = hash)
+            GetFileDetails()
+        Catch ex As Exception
+        Finally
+            TextBox1.Text = Nothing
+            Me.Invoke(Sub() GetFileDetails())
+        End Try
+    End Sub
+
+    Private Sub Virustotal_scan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
